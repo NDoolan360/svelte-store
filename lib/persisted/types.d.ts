770d76bf5d3759ee858450f5b9551e557c8e9a9c
NDoolan360 <n.doolan360@gmail.com>
2023-11-09 19:28:54 +1100

    Built lib

---
+++ lib/persisted/types.d.ts
@@ -0,0 +1,14 @@
+import { WritableLoadable } from '../async-stores/types.js';
+export type StorageType = 'LOCAL_STORAGE' | 'SESSION_STORAGE' | 'COOKIE';
+export type StorageOptions = {
+    reloadable?: true;
+    storageType?: StorageType | string;
+    consentLevel?: unknown;
+};
+interface Syncable<T> {
+    resync: () => Promise<T>;
+    clear: () => Promise<void>;
+    store: Syncable<T>;
+}
+export type Persisted<T> = Syncable<T> & WritableLoadable<T>;
+export {};
